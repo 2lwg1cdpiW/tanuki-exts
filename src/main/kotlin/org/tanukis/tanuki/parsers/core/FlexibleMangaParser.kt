c4453da7a26da12e4eaaf959ca1d596999f27bf7
2lwg1cdpiW <aymseygma@gmail.com>
2026-02-03 21:17:05 +0800

    Refactor FlexibleMangaParser for consistency

--- src/main/kotlin/org/tanukis/tanuki/parsers/core/FlexibleMangaParser.kt
+++ src/main/kotlin/org/tanukis/tanuki/parsers/core/FlexibleMangaParser.kt
@@ -16,79 +16,79 @@ import org.tanukis.tanuki.parsers.network.WebClient
 import org.tanukis.tanuki.parsers.util.*
 import java.util.*
 
+
 @Deprecated("Too complex. Use AbstractMangaParser instead")
 internal abstract class FlexibleMangaParser @InternalParsersApi constructor(
-	@property:InternalParsersApi val context: MangaLoaderContext,
-	final override val source: MangaParserSource,
+    @property:InternalParsersApi val context: MangaLoaderContext,
+    final override val source: MangaParserSource,
 ) : MangaParser {
 
-	override val config: MangaSourceConfig by lazy { context.getConfig(source) }
+    override val config: MangaSourceConfig by lazy { context.getConfig(source) }
 
-	open val sourceLocale: Locale
-		get() = if (source.locale.isEmpty()) Locale.ROOT else Locale(source.locale)
+    open val sourceLocale: Locale
+        get() = if (source.locale.isEmpty()) Locale.ROOT else Locale(source.locale)
 
-	protected open val userAgentKey: ConfigKey.UserAgent = ConfigKey.UserAgent(context.getDefaultUserAgent())
+    protected open val userAgentKey: ConfigKey.UserAgent = ConfigKey.UserAgent(context.getDefaultUserAgent())
 
-	final override val filterCapabilities: MangaListFilterCapabilities
-		get() = searchQueryCapabilities.toMangaListFilterCapabilities()
+    final override val filterCapabilities: MangaListFilterCapabilities
+        get() = searchQueryCapabilities.toMangaListFilterCapabilities()
 
-	protected val sourceContentRating: ContentRating?
-		get() = if (source.contentType == ContentType.HENTAI) {
-			ContentRating.ADULT
-		} else {
-			null
-		}
+    protected val sourceContentRating: ContentRating?
+        get() = if (source.contentType == ContentType.HENTAI) {
+            ContentRating.ADULT
+        } else {
+            null
+        }
 
-	final override val domain: String
-		get() = config[configKeyDomain]
+    final override val domain: String
+        get() = config[configKeyDomain]
 
-	@Deprecated("Override intercept() instead")
-	override fun getRequestHeaders(): Headers = Headers.Builder()
-		.add("User-Agent", config[userAgentKey])
-		.build()
+    @Deprecated("Override intercept() instead")
+    override fun getRequestHeaders(): Headers = Headers.Builder()
+        .add("User-Agent", config[userAgentKey])
+        .build()
 
-	/**
-	 * Used as fallback if value of `order` passed to [getList] is null
-	 */
-	open val defaultSortOrder: SortOrder
-		get() {
-			val supported = availableSortOrders
-			return SortOrder.entries.first { it in supported }
-		}
+    /**
+     * Used as fallback if value of `order` passed to [getList] is null
+     */
+    open val defaultSortOrder: SortOrder
+        get() {
+            val supported = availableSortOrders
+            return SortOrder.entries.first { it in supported }
+        }
 
-	@JvmField
-	protected val webClient: WebClient = OkHttpWebClient(context.httpClient, source)
+    protected open val webClient: WebClient = OkHttpWebClient(context.httpClient, source)
 
-	/**
-	 * Fetch direct link to the page image.
-	 */
-	override suspend fun getPageUrl(page: MangaPage): String = page.url.toAbsoluteUrl(domain)
+    /**
+     * Fetch direct link to the page image.
+     */
+    override suspend fun getPageUrl(page: MangaPage): String = page.url.toAbsoluteUrl(domain)
 
-	final override suspend fun getList(offset: Int, order: SortOrder, filter: MangaListFilter): List<Manga> {
-		return getList(convertToMangaSearchQuery(offset, order, filter))
-	}
+    final override suspend fun getList(offset: Int, order: SortOrder, filter: MangaListFilter): List<Manga> {
+        return getList(convertToMangaSearchQuery(offset, order, filter))
+    }
 
-	/**
-	 * Parse favicons from the main page of the source`s website
-	 */
-	override suspend fun getFavicons(): Favicons {
-		return FaviconParser(webClient, domain).parseFavicons()
-	}
+    /**
+     * Parse favicons from the main page of the source`s website
+     */
+    override suspend fun getFavicons(): Favicons {
+        return FaviconParser(webClient, domain).parseFavicons()
+    }
 
-	@CallSuper
-	override fun onCreateConfig(keys: MutableCollection<ConfigKey<*>>) {
-		keys.add(configKeyDomain)
-	}
+    @CallSuper
+    override fun onCreateConfig(keys: MutableCollection<ConfigKey<*>>) {
+        keys.add(configKeyDomain)
+    }
 
-	override suspend fun getRelatedManga(seed: Manga): List<Manga> {
-		return RelatedMangaFinder(listOf(this)).invoke(seed)
-	}
+    override suspend fun getRelatedManga(seed: Manga): List<Manga> {
+        return RelatedMangaFinder(listOf(this)).invoke(seed)
+    }
 
-	/**
-	 * Return [Manga] object by web link to it
-	 * @see [Manga.publicUrl]
-	 */
-	override suspend fun resolveLink(resolver: LinkResolver, link: HttpUrl): Manga? = null
+    /**
+     * Return [Manga] object by web link to it
+     * @see [Manga.publicUrl]
+     */
+    override suspend fun resolveLink(resolver: LinkResolver, link: HttpUrl): Manga? = null
 
-	override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(chain.request())
+    override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(chain.request())
 }
